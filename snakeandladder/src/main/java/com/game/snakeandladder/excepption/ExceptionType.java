21a05d38dbfb75ccfe196df06e25176486d9f400
sujata007 <priyadarshinee11@gmail.com>
2023-02-24 23:59:13 +0530

    Delete snakeandladder directory

--- snakeandladder/src/main/java/com/game/snakeandladder/excepption/ExceptionType.java
+++
@@ -1,5 +0,0 @@
-package com.game.snakeandladder.excepption;
-
-public enum ExceptionType {
-	POSITION_NOT_VALID, PLAYER_EXCEEDED,INCORRECT_PLAYER;
-}
--- snakeandladder/src/main/java/com/game/snakeandladder/service/PlayerService.java
+++
@@ -1,8 +0,0 @@
-package com.game.snakeandladder.service;
-
-import com.game.snakeandladder.model.Player;
-
-public interface PlayerService {
-
-	public int rollDice(Player playerName);
-}
